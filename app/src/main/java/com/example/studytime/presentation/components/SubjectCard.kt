a355955d55c28477406e5f7f27ee34865b5178e5
sandxx03 <sandralsmxx@gmail.com>
2024-08-04 11:52:57 +0800

    optimize imports

--- app/src/main/java/com/example/studytime/presentation/components/SubjectCard.kt
+++ app/src/main/java/com/example/studytime/presentation/components/SubjectCard.kt
@@ -11,15 +11,12 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.studytime.R
-import com.example.studytime.domain.model.Subject
 
 @Composable
 fun SubjectCard(
